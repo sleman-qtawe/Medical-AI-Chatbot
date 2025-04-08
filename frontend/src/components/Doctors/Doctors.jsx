41268adb93b5b47e7d149570abbebb3ecd0efa70
sleman-qtawe <slemanqtawe0@gmail.com>
2025-04-08 11:05:53 +0300

    s

--- frontend/src/components/Doctors/Doctors.jsx
+++ frontend/src/components/Doctors/Doctors.jsx
@@ -10,7 +10,7 @@ export default function Doctors() {
   const [selectedDoctor, setSelectedDoctor] = useState(null);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/Doctors")
+    fetch("http://127.0.0.1:5000/doctors")
       .then((res) => res.json())
       .then((data) => setDoctors(data));
   }, []);
